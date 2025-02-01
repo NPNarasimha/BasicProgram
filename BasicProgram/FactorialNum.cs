94698aa3dd2c1bba20fc1d9c121856401b279668
Np Narasimha <npkrish1122@gmail.com>
2025-02-01 12:20:23 +0530

    updated feature

---
+++ BasicProgram/FactorialNum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    internal class FactorialNum
+    {
+        public static void factorial()
+        {   Console.WriteLine("Enter a number to find factorial : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                fact *= i;
+
+            }
+            Console.WriteLine("Factorial of a number is : "+ fact);
+        }
+    }
+}
---
+++ BasicProgram/PrintNumInRecursion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    internal class PrintNumInRecursion
+    {
+        public static void PrintNumRecursion(int n)
+        {
+            
+            if (n > 100)
+            {
+                return ;
+            }
+            else
+            {
+                Console.Write(n+" ");
+                PrintNumRecursion(n+1);
+            }
+
+        }
+       
+    }
+}
