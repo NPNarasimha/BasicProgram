ea94a1c95b59e676ddb755dcfd4ee7d39a57426a
Np Narasimha <npkrish1122@gmail.com>
2025-02-04 07:57:35 +0530

    Update commit

--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -4,21 +4,10 @@ namespace BasicProgram
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //PrintNames.Names();
-            //SumDiffSqrt.SumDiff();
-            //Upper_LowerCase.PrintPlace();
-            //PrintNum.Numbers();
-            //AvgNumbers.AverageNums();
-            //PerimeterAreaCircle.CalculateCircle();
-            //PrintDate.date();
-            //NumPrintDiv7.printDiv7();
-            //CmdArgsPassing.CmdArgs(args);
-            //FactorialNum.factorial();
-            //PrintNumInRecursion.PrintNumRecursion(1);
-            //ConvertNumToString.NumToString();
-            CheckingChars.CheckChars();
+            //GradesOfStudent.Grades();
+            TemparatureCal.Temparature();
         }
     }
 }
---
+++ BasicProgram/TemparatureCal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    internal class TemparatureCal
+    {
+        public static void Temparature()
+        {
+            Console.WriteLine("Enter the temparature");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the 1 to calculate in celsius and 2 for calculate in Fahrenheit");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            if (userChoice == 1) { 
+            double celsius = (temp - 32) * 5 / 9;
+                Console.WriteLine("The Temparature in Celsius : " + celsius);
+            }
+            else if (userChoice == 2)
+            {
+                double fahrenheit = (temp * 9 / 5) + 32;
+                Console.WriteLine("The Temparature in Fahrenheit : " + fahrenheit);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Choice");
+            }
+            
+        }
+    }
+}
