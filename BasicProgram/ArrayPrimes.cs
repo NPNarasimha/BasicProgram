c48f3ae964016109791a84f7ac746220bee334d2
Np Narasimha <npkrish1122@gmail.com>
2025-02-05 16:15:22 +0530

    update by np

---
+++ BasicProgram/ArrayPrimes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    internal class ArrayPrimes
+    {
+        public static void Primes()
+        {
+            Console.WriteLine("Enter the prime Numbers");
+            int [] arr=new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i =0;i<=arr.Length-1;i++)
+            {
+                int count = 0;
+                for (int j = 2; j <= arr[i]; j++)
+                {
+                    if (arr[i] % j == 0) { 
+                        count++;
+                    }
+                }
+                if (count < 2)
+                {
+                    Console.WriteLine("Prime number is : " + arr[i]);
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ BasicProgram/RepeatEle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    internal class RepeatEle
+    { 
+        public static void RepeatElement()
+        {
+            int[] arr = { 1, 2, 3, 0, 7, 3, 21, 1, 3, 4, 4 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j <= arr.Length - 1; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        Console.WriteLine("First Repeat Element : " + arr[i]);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
